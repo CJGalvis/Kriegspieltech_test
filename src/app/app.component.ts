3c6ac1145191b928f405b63c5f8e06ec49952e85
Camilo Galvis <cjgalvis15@gmail.com>
2024-04-03 12:40:50 -0500

    implemente test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ServiceService } from './service.service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Kriegspieltech';
+  public userControl = new FormControl();
+  public userResponse: any;
+
+  constructor(private api: ServiceService) {}
+
+  async getUser() {
+    try {
+      const dni = this.userControl.value;
+      const response = await firstValueFrom(this.api.usersAPI(dni));
+      if (response) {
+        this.userResponse = response;
+      }
+    } catch (error) {
+      this.userResponse = null;
+    }
+  }
 }
